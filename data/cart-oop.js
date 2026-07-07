4891e50838807b93edff6b9f5022ec88c4239d85
kush19x <kushagra19dixit@gmail.com>
2026-07-08 03:55:51 +0530

    Created OOP Version of Cart

---
+++ data/cart-oop.js
@@ -0,0 +1,115 @@
+function Cart(localStorageKey) {
+  const cart = {
+  cartItems: undefined,
+
+  loadFromStorage() {
+    this.cartItems = JSON.parse(localStorage.getItem(localStorageKey));
+
+    if(!this.cartItems){
+      this.cartItems = [{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2,
+        deliveryOptionId:'1'
+      }, {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1,
+        deliveryOptionId:'2'
+      }];
+    } 
+  },
+
+  saveToStorage() {
+    localStorage.setItem(localStorageKey, JSON.stringify(this.cartItems));
+  },
+
+  addToCart(productId,quantity=1) {
+    const matchingcartItem=this.cartItems.find(product => product.productId===productId);
+
+      if(matchingcartItem)
+      {
+        matchingcartItem.quantity+=quantity;
+      }
+      else
+      {
+        this.cartItems.push({
+        productId: productId,
+        quantity: quantity,
+        deliveryOptionId:'1'
+      });
+      }
+
+      this.saveToStorage();
+  },
+
+  calculateCartQuantity() {
+    let cartQuantity=0;
+
+    this.cartItems
+      .forEach((cartItem) => {
+      cartQuantity+=cartItem.quantity;
+    });
+
+    return cartQuantity;
+  },
+
+  updateCartQuantity () {
+    const cartQuantity=this.calculateCartQuantity();
+
+    document.querySelector('.js-cart-quantity')
+      .innerHTML=cartQuantity;
+    this.saveToStorage();
+  },
+
+  updateProductQuantity (productId, newQuantity)
+  {
+    this.cartItems.forEach( (matchingItem) => {
+
+      if(matchingItem.productId===productId)
+        matchingItem.quantity=newQuantity;
+    });
+    this.saveToStorage();
+  },
+  
+  removeFromCart (productId) {
+    const newCart=[];
+
+    this.cartItems.forEach( (cartItem) => {
+      if(cartItem.productId!=productId)
+      {
+        newCart.push(cartItem);
+      }
+    });
+
+    this.cartItems=newCart;
+
+    this.saveToStorage();
+  },
+
+  updateDeliveryOption(productId, deliveryOptionId){
+  let matchingItem;
+
+  this.cartItems.forEach( (cartItem) =>{
+    if(productId===cartItem.productId)
+    {
+      matchingItem=cartItem;
+    }
+  });
+
+    matchingItem.deliveryOptionId=deliveryOptionId;
+
+    this.saveToStorage();
+  }
+  };
+
+  return cart;
+}
+
+const cart=Cart('cart-oop');
+const businessCart=Cart('cart-business');
+
+cart.loadFromStorage();
+
+businessCart.loadFromStorage();
+
+console.log(cart);
+console.log(businessCart);
