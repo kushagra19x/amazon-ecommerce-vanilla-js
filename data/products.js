913a950fedcb457118617d41e265ed7f02e7b57c
kush19x <kushagra19dixit@gmail.com>
2026-07-17 16:57:53 +0530

    Added Instructions and Warranty Links to Appliance Products

--- data/products.js
+++ data/products.js
@@ -82,6 +82,24 @@ const object2 = {
 object2.method(); this will return undefined
 */
 
+class Appliance extends Product {
+  instructionsLink;
+  warrantyLink;
+
+  constructor(productDetails)
+  {
+    super(productDetails);
+    this.instructionsLink=productDetails.instructionsLink;
+    this.warrantyLink=productDetails.warrantyLink;
+  }
+
+extraInfoHTML() {
+  return `
+    <a href="${this.instructionsLink}" target="_blank">Instructions</a>
+    <a href="${this.warrantyLink}" target="_blank">Warranty</a>
+  `;
+}
+};
 
 export const products = [
   {
@@ -143,7 +161,10 @@ export const products = [
       "toaster",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type:"appliance",
+    instructionsLink:"images/appliance-instructions.png",
+    warrantyLink:"images/appliance-warranty.png"
   },
   {
     id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
@@ -328,7 +349,10 @@ export const products = [
       "water boiler",
       "appliances",
       "kitchen"
-    ]
+    ],
+    type:"appliance",
+    instructionsLink:"images/appliance-instructions.png",
+    warrantyLink:"images/appliance-warranty.png"  
   },
   {
     id: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
@@ -633,7 +657,10 @@ export const products = [
       "coffeemakers",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type:"appliance",
+    instructionsLink:"images/appliance-instructions.png",
+    warrantyLink:"images/appliance-warranty.png"
   },
   {
     id: "02e3a47e-dd68-467e-9f71-8bf6f723fdae",
@@ -693,7 +720,10 @@ export const products = [
       "food blenders",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type:"appliance",
+    instructionsLink:"images/appliance-instructions.png",
+    warrantyLink:"images/appliance-warranty.png"
   },
   {
     id: "36c64692-677f-4f58-b5ec-0dc2cf109e27",
@@ -748,5 +778,9 @@ export const products = [
     return new Clothing(productDetails);
   }
 
+  else if(productDetails.type=='appliance')
+  {
+    return new Appliance(productDetails);
+  }
   return new Product(productDetails);
 });
